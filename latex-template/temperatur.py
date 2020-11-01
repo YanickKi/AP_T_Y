089ac3d3bc593580e667e7afe045ca9463c9d4d5
Yanick Kind <yanick.kind@tu-dortmund.de>
2020-11-01 19:57:21 +0100

    debugging

--- latex-template/temperatur.py
+++ latex-template/temperatur.py
@@ -22,8 +22,8 @@ for name2, value2, error2 in zip('abc', params2, errors2):
 
 plt.plot(t, T1, '.', label='$T_1$')
 plt.plot(t, T2, '.', label='$T_2$')
-plt.plot(x, params1[0] * x**2 + params1[1] * x + params1[2], label = 'Ausgleichsgerade für $T_1$')
-plt.plot(x, params2[0] * x**2 + params2[1] * x + params2[2], label = 'Ausgleichsgerade für $T_2$')
+plt.plot(x, params1[0] * x**2 + params1[1] * x + params1[2], label = 'Ausgleichskurve für $T_1$')
+plt.plot(x, params2[0] * x**2 + params2[1] * x + params2[2], label = 'Ausgleichskurve für $T_2$')
 plt.xlabel(r'$t\:/\: \si{\second}$')
 plt.ylabel(r'$t \:/\: \si{\kelvin}$')
 plt.legend(loc='best')
