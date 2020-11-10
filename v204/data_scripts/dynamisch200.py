f8ea3022edf1365774ea9c04ee4ede3603417939
Yanick Kind <yanick.kind@tu-dortmund.de>
2020-11-10 18:08:33 +0100

    included scripts and data

---
+++ v204/data_scripts/dynamisch200.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t, T1, T4, T5, T8, T2, T3, T6, T7 = np.genfromtxt('data_scripts/dynamisch200.txt', unpack = True)
+
+
+plt.plot(t*2, T1, label = 'T1')
+plt.plot(t*2, T2, label = 'T2')
+
+plt.xlabel('t / s')
+plt.ylabel('T / C')
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/dynamisch200.pdf')
--- v204/data_scripts/statisch.py
+++ v204/data_scripts/statisch.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t, T1, T4, T5, T8, T2, T3, T6, T7 = np.genfromtxt('statisch.txt', unpack = True)
+t, T1, T4, T5, T8, T2, T3, T6, T7 = np.genfromtxt('data_scripts/statisch.txt', unpack = True)
 
 plt.plot(t / 10, T1, label = 'T1')
 plt.plot(t / 10, T4, label = 'T4')
@@ -11,5 +11,5 @@ plt.plot(t / 10, T8, label = 'T8')
 plt.xlabel('t / s')
 plt.ylabel('T / C')
 plt.legend()
-plt.tight_layout()
-plt.savefig('Test.pdf')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/statisch.pdf')
