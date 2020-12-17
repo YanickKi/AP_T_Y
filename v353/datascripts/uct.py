3eb3d0953c279fc6fc6bb36a57dd5cc5770d7307
Yanick Kind <yanick.kind@tu-dortmund.de>
2020-12-17 08:25:04 +0100

    begon eevaluation

--- v353/datascripts/uct.py
+++ v353/datascripts/uct.py
@@ -5,7 +5,7 @@ t, U = np.genfromtxt('datascripts/uct.txt', unpack = True)
 U       += 3.1
 t       *= 0.2
 tf      = np.delete(t, -1) 
-lnu     = np.log(U)
+lnu     = np.log(U)/3.1
 lnuf    = np.delete(lnu, -1)
 
 params1, covariance_matrix1 = np.polyfit(t, lnu, deg=1, cov=True)
@@ -17,16 +17,16 @@ errors2= np.sqrt(np.diag(covariance_matrix2))
 x = np.linspace(t[0], t[-1])
 y = np.linspace(tf[0], tf[-1])
 
-plt.plot(x, x * params1[0] + params1[1], label = 'Kritische Ausgleichsgerade')
-plt.plot(y, y * params2[0] + params2[1], label = 'Ausgleichsgerade')
+plt.plot(x, x * params1[0] + params1[1], label = 'Gesamte Ausgleichsgerade')
+plt.plot(y, y * params2[0] + params2[1], label = 'Kritische Ausgleichsgerade')
 plt.plot(t, lnu, '.',  label = 'Messdaten')
 
 plt.xlabel(r'$t \mathbin{/} \si{\milli\second}$')
-plt.ylabel(r'$U \mathbin{/} \si{\volt}$')
+plt.ylabel(r'$\ln \left (U / U_0 \right )$')
 
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.legend()
 plt.savefig('build/uct.pdf')
 
-print(f'Steigung der Kritischen Gerade {params1[0]}')
-print(f'Steigung der idealisierten Gerade {params2[0]}')
+print(f'Steigung der gesamten Gerade {params1[0]} pm {np.sqrt(np.diag(covariance_matrix1))}')
+print(f'Steigung der kritischen Gerade {params2[0]} pm {np.sqrt(np.diag(covariance_matrix2))}')
