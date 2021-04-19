4d685d454b41b51c79d4a749289d6e52107ca79f
Yanick Kind <yanick.kind@tu-dortmund.de>
2021-04-19 15:02:23 +0200

    protocoll finished

--- v504/data_scripts/charcurve.py
+++ v504/data_scripts/charcurve.py
@@ -19,10 +19,12 @@ I23 *=20
 I24 *=20
 I25 *=20
 
+i = 1
 
-plt.plot(U22, I22,'.', label = r'$I_{\text{A}1}  \; ( I_\text{H} = \SI{2.0}{\ampere} \text{,} \; U_\text{H} = \SI{4.0}  {\volt})   $')
+
+plt.plot(U20, I20,'.', label = r'$I_{\text{A}1}  \; ( I_\text{H} = \SI{2.0}{\ampere} \text{,} \; U_\text{H} = \SI{4.0}  {\volt})   $')
 plt.plot(U21, I21,'.', label = r'$I_{\text{A}2}  \; ( I_\text{H} = \SI{2.1}{\ampere} \text{,} \; U_\text{H} = \SI{4.1}{\volt})     $')
-plt.plot(U20, I20,'.', label = r'$I_{\text{A}3}  \; ( I_\text{H} = \SI{2.2}{\ampere} \text{,} \; U_\text{H} = \SI{4.2}{\volt})     $')
+plt.plot(U22, I22,'.', label = r'$I_{\text{A}3}  \; ( I_\text{H} = \SI{2.2}{\ampere} \text{,} \; U_\text{H} = \SI{4.2}{\volt})     $')
 plt.plot(U23, I23,'.', label = r'$I_{\text{A}4}  \; ( I_\text{H} = \SI{2.3}{\ampere} \text{,} \; U_\text{H} = \SI{4.7}{\volt})     $')
 plt.plot(U24, I24,'.', label = r'$I_{\text{A}5}  \; ( I_\text{H} = \SI{2.4}{\ampere} \text{,} \; U_\text{H} = \SI{5.0}  {\volt})   $')
 plt.plot(U25, I25,'.', label = r'$I_{\text{A}6}  \; ( I_\text{H} = \SI{2.5}{\ampere} \text{,} \; U_\text{H} = \SI{5.3}{\volt})     $')
